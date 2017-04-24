692416add39e52f9fa38186a4fb058c3a07c885d
Ulysses Pabuna <upabuna@globeandmail.com>
2017-04-24 15:01:00 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ PoWaSettings.promo = {
 
     let template = `
     <div class="c-video__button c-video__button--play powa-shot-image powa-shot-click powa-shot-touch powa-shot-touch-background">
-      <button class="c-button c-button--dark c-button--video powa-shot-play-btn powa-shot-hover powa-shot-click powa-shot-click-play" aria-label="Play Video">
+      <button class="c-button c-button--dark c-button--video powa-shot-click powa-shot-click-play" aria-label="Play Video">
         <svg class="c-button__icon" role="img" pointer-events="none" focusable="false" aria-hidden="true" role="presentation" pointer-events="none">
           <use xlink:href="/ux-pattern-library/assets/patterns/images/sprite.svg#icon-play"></use>
         </svg>
